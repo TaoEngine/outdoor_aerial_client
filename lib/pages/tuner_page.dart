2d3b7dad21c77d5391b86088ecb38eeabd9a5966
TaoEngine <taoengine@qq.com>
2025-08-09 14:45:41 +0800

    加入无节目界面

--- lib/pages/tuner_page.dart
+++ lib/pages/tuner_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 
 class TunerPage extends StatelessWidget {
   const TunerPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return NoProgramPage();
+  }
+}
+
+class NoProgramPage extends StatelessWidget {
+  const NoProgramPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        icon: Icon(TablerIcons.chart_arcs),
+        label: Text("搜索节目"),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      body: Center(
+        child: Column(
+          spacing: 10,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(TablerIcons.antenna_off, size: 100),
+            Text(
+              "没有节目哦！试着搜索一些...",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
