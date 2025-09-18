59b2f719eb3fc0f764286b760bb38d9819f209c3
TaoEngine <taoengine@qq.com>
2025-09-18 23:31:26 +0800

    改变项目名称

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:go_router/go_router.dart';
 
-import 'package:outline_aerial_client/pages/main_page.dart';
-import 'package:outline_aerial_client/pages/splash_page.dart';
+import 'package:outdoor_aerial_client/pages/main_page.dart';
+import 'package:outdoor_aerial_client/pages/splash_page.dart';
 
 void main() {
   runApp(const MainApp());
