23cdb93b3fbffe288865a0213f7c7e9f458b4cad
TaoEngine <taoengine@qq.com>
2025-09-19 13:44:02 +0800

    Tuner界面加入了节目选择

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:outdoor_aerial_client/pages/tuner/tuner_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -27,11 +28,30 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Icon(TablerIcons.antenna),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(TablerIcons.search),
+          tooltip: "查找",
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsetsGeometry.all(8),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(TablerIcons.radar),
+              tooltip: "搜台",
+            ),
+          ),
+        ],
+      ),
       body: TabBarView(
         controller: _tabController,
         physics: const NeverScrollableScrollPhysics(),
         children: <Widget>[
-          Placeholder(),
+          TunerPage(),
           Placeholder(),
           Placeholder(),
           Placeholder(),
--- lib/pages/tuner/tuner_page.dart
+++ lib/pages/tuner/tuner_page.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
 
-class TunerPage extends StatelessWidget {
+class TunerPage extends StatefulWidget {
   const TunerPage({super.key});
 
   @override
+  State<TunerPage> createState() => _TunerPageState();
+}
+
+class _TunerPageState extends State<TunerPage> with TickerProviderStateMixin {
+  late final TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 8, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return ListView(
+      children: [
+        TabBar(
+          controller: _tabController,
+          isScrollable: true,
+          tabs: [
+            Tab(
+              icon: Image.network(
+                "https://ytmedia.radio.cn/CCYT%2F202407%2F23%2F20%2FySDxekAkYODX0CiElAjIfhrzA5Af0ugnlKkGHhTboa2024072320988.jpg",
+              ),
+            ),
+            Tab(
+              icon: Image.network(
+                "https://ytmedia.radio.cn/CCYT%2F202407%2F23%2F20%2FPIaFilaog3C7OCpvyjBJ1soT40EMErj1y2024072320478.jpg",
+              ),
+            ),
+            Tab(
+              icon: Image.network(
+                "https://ytmedia.radio.cn/CCYT%2F202307%2F19%2F14%2Fr40Lbd5da1p3BFPDF0JuyNmJILvsZ5DLnoRWk8Cdfzy2023071914352.jpg",
+              ),
+            ),
+            Tab(
+              icon: Image.network(
+                "https://ytmedia.radio.cn/CCYT%2F202407%2F23%2F20%2Fe3Tt7oCvlk7Jqvc9quZuStEXorRqMLa8ysWS2024072320460.jpg",
+              ),
+            ),
+            Tab(
+              icon: Image.network(
+                "https://ytmedia.radio.cn/CCYT%2F202407%2F23%2F20%2FJwbi4NDQ2024072320225.png",
+              ),
+            ),
+            Tab(
+              icon: Image.network(
+                "https://ytmedia.radio.cn/CCYT%2F202407%2F23%2F20%2FTWUnd3Dx91UHm2024072320530.jpg",
+              ),
+            ),
+            Tab(
+              icon: Image.network(
+                "https://ytmedia.radio.cn/CCYT%2F202407%2F23%2F21%2FqrAYKFsw1eT1zBqivRaAw7wqDTub9JBBRe2024072321762.PNG",
+              ),
+            ),
+            Tab(
+              icon: Image.network(
+                "https://ytmedia.radio.cn/CCYT%2F202407%2F23%2F20%2F7UD3U0bncsbHFPya0MF2024072320388.jpg",
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
