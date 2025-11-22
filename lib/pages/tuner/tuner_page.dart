f1d55862077aea259853de3eaa5552691e9a5774
TaoEngine <taoengine@qq.com>
2025-11-22 21:39:19 +0800

    加入Windows支持

--- lib/pages/tuner/tuner_page.dart
+++ lib/pages/tuner/tuner_page.dart
@@ -12,15 +12,7 @@ class TunerPage extends StatefulWidget {
 class _TunerPageState extends State<TunerPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        SelectionProgramTitle(),
-        SelectionProgramView(),
-        Divider(),
-        FavoriteProgramTitle(),
-        Padding(padding: const EdgeInsets.all(10), child: FavoriteProgramView()),
-      ],
-    );
+    return ListView(children: [SelectionProgramTitle(), SelectionProgramView(), Divider(), FavoriteProgramTitle()]);
   }
 }
 
@@ -127,66 +119,6 @@ class FavoriteProgramTitle extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class FavoriteProgramView extends StatelessWidget {
-  const FavoriteProgramView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      mainAxisSpacing: 20,
-      crossAxisSpacing: 20,
-      childAspectRatio: 1,
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      children: List.generate(1, (int index) {
-        return Card.outlined(
-          clipBehavior: Clip.antiAlias,
-          child: InkWell(
-            onTap: () {
-              // TODO 点击卡片转至详情
-              GoRouter.of(context).goNamed("PlayPage");
-            },
-            child: Stack(
-              children: [
-                Ink.image(
-                  image: NetworkImage(
-                    "https://ytmedia.radio.cn/CCYT%2F202407%2F23%2F21%2FqrAYKFsw1eT1zBqivRaAw7wqDTub9JBBRe2024072321762.PNG",
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: EdgeInsetsGeometry.all(10),
-                    child: Text(
-                      "话题：ABCDEFG",
-                      style: Theme.of(
-                        context,
-                      ).textTheme.displaySmall?.copyWith(color: Theme.of(context).colorScheme.primaryContainer),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Padding(
-                    padding: EdgeInsetsGeometry.all(10),
-                    child: IconButton.filledTonal(
-                      onPressed: () {
-                        // TODO 点击按钮播放节目
-                      },
-                      icon: Icon(TablerIcons.player_play),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      }),
     );
   }
 }
