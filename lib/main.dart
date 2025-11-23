0529572b2e18ab44ae7b56dcb97aaba04b2dd929
TaoEngine <taoengine@qq.com>
2025-11-23 09:07:50 +0800

    调整包顺序

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:m3e_collection/m3e_collection.dart';
