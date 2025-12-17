64de12c8dfa0b3a08576d53173d122567aeb1475
TaoEngine <taoengine@qq.com>
2025-12-17 19:11:15 +0800

    移植protobuf数据结构出去

--- lib/providers/broadcast_programs.dart
+++ lib/providers/broadcast_programs.dart
@@ -1,16 +1,18 @@
 import 'dart:async';
 
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:outdoor_aerial_client/models/broadcast_datetime.pb.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'package:outdoor_aerial_client/models/broadcast_program.pb.dart';
 
 class BroadcastProgramsNotifier extends AsyncNotifier<BroadcastPrograms> {
-  /// OutdoorAerial 服务器地址
-  final String host;
+  // /// OutdoorAerial 服务器地址
+  // final String host;
 
-  BroadcastProgramsNotifier({required this.host});
+  // BroadcastProgramsNotifier({required this.host});
 
   WebSocketChannel? _channel;
   StreamSubscription? _subscription;
@@ -26,7 +28,7 @@ class BroadcastProgramsNotifier extends AsyncNotifier<BroadcastPrograms> {
     try {
       // 登录 OutdoorAerial 服务器
       _channel = WebSocketChannel.connect(
-        Uri(scheme: "ws", host: host, path: "program"),
+        Uri(scheme: "ws", host: "127.0.0.1:8908", path: "program"),
       );
       state = const AsyncValue.data(
         BroadcastPrograms(status: ConnectStatus.connecting),
@@ -34,6 +36,9 @@ class BroadcastProgramsNotifier extends AsyncNotifier<BroadcastPrograms> {
 
       _subscription = _channel!.stream.listen(
         (data) {
+          final date = DateData.fromBuffer(data);
+          debugPrint("${date.day}");
+
           final currentState =
               state.value ??
               const BroadcastPrograms(status: ConnectStatus.disconnect);
