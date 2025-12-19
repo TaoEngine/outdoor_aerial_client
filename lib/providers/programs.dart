3f56e7f4243a0525fe7d27cc6f34db45468b2fd6
TaoEngine <taoengine@qq.com>
2025-12-19 22:16:14 +0800

    简化provider代码

--- lib/providers/programs.dart
+++ lib/providers/programs.dart
@@ -6,18 +6,10 @@ import 'package:outdoor_aerial_client/models/program/program.pb.dart';
 import 'package:outdoor_aerial_client/models/program/single.pb.dart';
 import 'package:outdoor_aerial_client/services/programs.dart';
 
-final _programsProvider = Provider<ProgramsGetService>((ref) {
-  return ProgramsGetService(address: 'localhost');
-});
-
-final _todayProgramsProvider = Provider<TodayProgramsService>((ref) {
-  return TodayProgramsService(address: 'localhost');
-});
-
 class ProgramsNotifier extends AsyncNotifier<List<Program>> {
   @override
   FutureOr<List<Program>> build() async {
-    final service = ref.read(_programsProvider);
+    final service = ProgramsGetService(address: 'localhost');
     // 获取所有节目
     final programs = await service.get();
 
@@ -28,13 +20,13 @@ class ProgramsNotifier extends AsyncNotifier<List<Program>> {
 class TodayProgramsNotifier extends AsyncNotifier<List<SingleProgram>> {
   @override
   FutureOr<List<SingleProgram>> build() {
-    final service = ref.read(_todayProgramsProvider);
+    final service = TodayProgramsService(address: 'localhost');
     final subscription = service.stream().listen((program) {
       // 节目更新核心机制
       state = state.whenData((list) => [...list, program]);
     });
 
-    // 及时停止连接
+    // 创建 “停止连接”
     ref.onDispose(() {
       service.close();
       subscription.cancel();
