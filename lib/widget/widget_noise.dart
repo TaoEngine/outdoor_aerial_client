7a258c3301a288af89858754ce3c8d59eed580f1
TaoEngine <taoengine@qq.com>
2026-01-02 18:54:39 +0800

    小更改

--- lib/widget/widget_noise.dart
+++ lib/widget/widget_noise.dart
@@ -7,25 +7,23 @@ class NoisePainter extends CustomPainter {
 
   NoisePainter({required this.context});
 
-  final Random _random = Random.secure();
+  /// 随机数生成
+  final Random _random = Random();
 
   @override
   void paint(Canvas canvas, Size size) {
     // 限定范围
     final maxWidth = size.width.round();
     final maxHeight = size.height.round();
-    final maxDots = ((maxWidth * maxHeight / 16) * 0.6).round();
+    final maxDots = ((maxWidth * maxHeight / 2) * 0.1).round();
 
-    final backgroundPaint = Paint()
-      ..color = Theme.of(context).colorScheme.secondaryContainer;
-    final foregroundPaint = Paint()
-      ..color = Theme.of(context).colorScheme.onPrimary;
+    final paint = Paint()
+      ..color = Theme.of(context).colorScheme.secondary;
 
-    canvas.drawRect(Offset.zero & size, backgroundPaint);
     for (var i = 0; i < maxDots; i++) {
       final x = _random.nextInt(maxWidth).toDouble();
       final y = _random.nextInt(maxHeight).toDouble();
-      canvas.drawRect(.fromLTWH(x, y, 4, 4), foregroundPaint);
+      canvas.drawCircle(Offset(x, y), 1, paint);
     }
   }
 
