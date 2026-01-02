1625e20d30b7bf9595632f0c48d2576311c0001d
TaoEngine <taoengine@qq.com>
2026-01-02 17:09:05 +0800

    编写了取流播放广播直播的功能

---
+++ lib/service/service_broadcast.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter_soloud/flutter_soloud.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class BroadcastService {
+  /// Outdoor Aerial 的服务器地址
+  final String address;
+
+  /// 是否启用 Wss 安全访问
+  final bool isWss;
+
+  /// 广播音频播放服务
+  BroadcastService({required this.address, required this.isWss});
+
+  /// 指定的地址去执行取流任务
+  static const path = 'broadcast';
+
+  /// SoLoud 相关接口
+  AudioSource? _stream;
+
+  /// SoLoud 音频控制器
+  SoundHandle? _handle;
+
+  /// WebSocket 相关接口
+  WebSocketChannel? _channel;
+
+  /// 推流相关服务订阅
+  StreamSubscription? _subscription;
+
+  /// 标识广播流是否已静音
+  bool _isMuted = false;
+
+  /// 从 Outdoor Aerial 服务器中取流
+  Future<void> broadcast() async {
+    if (!SoLoud.instance.isInitialized) {
+      await SoLoud.instance.init();
+    }
+
+    try {
+      _stream = SoLoud.instance.setBufferStream(
+        bufferingType: .released,
+        channels: Channels.stereo,
+        format: BufferType.s16le,
+        bufferingTimeNeeds: 0.010,
+        sampleRate: 44100,
+      );
+    } catch (e) {
+      rethrow;
+    }
+
+    try {
+      _channel = WebSocketChannel.connect(switch (isWss) {
+        false => Uri(scheme: 'ws', host: address, port: 8908, path: path),
+        true => Uri(scheme: 'ws', host: address, port: 8908, path: path),
+      });
+      await _channel?.ready;
+    } catch (e) {
+      rethrow;
+    }
+
+    _handle = await SoLoud.instance.play(_stream!);
+
+    _subscription = _channel?.stream.listen((message) {
+      if (message is List<int>) {
+        final data = Uint8List.fromList(message);
+        if (_stream != null) {
+          SoLoud.instance.addAudioDataStream(_stream!, data);
+        }
+      }
+    });
+  }
+
+  /// 终止取流操作并销毁当前服务
+  Future<void> dispose() async {
+    await _subscription?.cancel();
+    _subscription = null;
+
+    await _channel?.sink.close();
+    _channel = null;
+
+    if (_handle != null) {
+      await SoLoud.instance.stop(_handle!);
+      _handle = null;
+    }
+
+    if (_stream != null) {
+      await SoLoud.instance.disposeSource(_stream!);
+      _stream = null;
+    }
+  }
+
+  /// 广播音频是否静音
+  bool get isMuted => _isMuted;
+
+  /// 对广播音频进行静音（不影响推流播放）
+  void mute() {
+    if (_handle != null) {
+      _isMuted = !_isMuted;
+      if (_isMuted) {
+        SoLoud.instance.setVolume(_handle!, 0);
+      } else {
+        SoLoud.instance.setVolume(_handle!, 1);
+      }
+    }
+  }
+}
---
+++ lib/widget/widget_no_programs.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:outdoor_aerial_client/widget/widget_noise.dart';
+
+class NoProgramsWidget extends StatefulWidget {
+  const NoProgramsWidget({super.key});
+
+  @override
+  State<NoProgramsWidget> createState() => _NoProgramsWidgetState();
+}
+
+class _NoProgramsWidgetState extends State<NoProgramsWidget>
+    with SingleTickerProviderStateMixin {
+  /// 无信号的画面显示动画
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // 快速闪烁的效果动画用来模拟电视无信号的画面
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return CustomPaint(
+            painter: NoisePainter(context: context),
+            child: child,
+          );
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 72,
+            children: [
+              Transform.scale(
+                scale: 3,
+                child: const CircleAvatar(child: Icon(TablerIcons.antenna_off)),
+              ),
+              Text(
+                '没有节目欸，搜索电台试试看',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
